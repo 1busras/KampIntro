77889ca69110b3d30ee7809299e040705067e0b5
Büşra Şahin <busrasahin0@outlook.com>
2021-01-21 18:13:01 +0300

    20.01.2021

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            // sayi1 30
+            // sayısal veri tipleri int decimol float double bool = değer tip 
+            // arroy class interface = referans tip 
+            // arroy in tipi önemli değil 
+            // stack and heap var 
+            // değer tip stock da 
+            // new yeni bir yer
+            // int arroy olduğunda referans eşitleniyor
+            // 
+
+        }
+    }
+}
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        //naming convention - isimlendirme - normal parentez varsa methods
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi");
+
+        }
+        
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi:" + urunAdi);
+        }
+
+
+
+
+    }
+}
